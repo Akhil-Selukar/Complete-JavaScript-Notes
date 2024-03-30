d56b4cd0223a3c691ae80c79f28e809035b8ae51
Akhil <akhilselukar.work@gmail.com>
2024-03-30 14:33:15 +0530

    slider implementation

--- 44-Advanced DOM/44.13-Slider component/script.js
+++ 44-Advanced DOM/44.13-Slider component/script.js
@@ -222,8 +222,28 @@ imageTargets.forEach((image) => imageObserver.observe(image));
 const slides = document.querySelectorAll(".slide");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
 
 let currentSlide = 0;
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
 
 const moveToSlide = function (slideNumber) {
   slides.forEach((slide, i) => {
@@ -239,6 +259,7 @@ const nextSlide = function () {
   }
 
   moveToSlide(currentSlide);
+  activateDot(currentSlide);
 };
 
 const previousSlide = function () {
@@ -248,9 +269,25 @@ const previousSlide = function () {
     currentSlide--;
   }
   moveToSlide(currentSlide);
+  activateDot(currentSlide);
 };
 
 moveToSlide(0);
+createDots();
+activateDot(0);
 
 btnRight.addEventListener("click", nextSlide);
 btnLeft.addEventListener("click", previousSlide);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") nextSlide();
+  e.key === "ArrowLeft" && previousSlide();
+});
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const slide = e.target.dataset.slide;
+    moveToSlide(slide);
+    activateDot(slide);
+  }
+});
